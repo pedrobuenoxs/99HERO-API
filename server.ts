f5c86bad99a3cfbd89fef322d2985ceaaf242fe3
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-21 20:39:58 -0300

    initial commit

---
+++ server.ts
@@ -0,0 +1,6 @@
+import app from "./src/app";
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log("Server running");
+});
---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+require("dotenv").config();
+import mongoose from "mongoose";
+
+const DB_LOGIN = process.env.DB_LOGIN;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_LOGIN}:${DB_PASSWORD}@dev-challenge.wwuxf6z.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Db connected");
+  })
+  .catch((err) => {
+    console.log("Error while connecting database::", err);
+  });
+
+app.use(express.json());
+export default app;
