134e8f72fd037cc18912d079d413fbc27400af7c
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-24 18:34:48 -0300

    add get all hero tests

---
+++ src/routes/GetAllHero.route.test.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import app from "../app";
+import DTOHero from "../DTO/hero.dto";
+import { HeroRepository } from "../repository/hero.repository";
+const Repository = new HeroRepository();
+beforeAll((done) => {
+  done();
+});
+afterEach(async () => {
+  await Repository.deleteAll();
+});
+
+afterAll((done) => {
+  // Closing the DB connection allows Jest to exit successfully.
+  mongoose.connection.close();
+  done();
+});
+
+describe("Hero GET Route", () => {
+  test("Should return 200 as status code and list all heros and correct properties", async () => {
+    const response = await request(app).get("/api/hero");
+    response.body.forEach((hero: DTOHero) => {
+      expect(hero).toHaveProperty("id");
+      expect(hero).toHaveProperty("heroName");
+      expect(hero).toHaveProperty("cities");
+      expect(hero).toHaveProperty("disasters");
+      expect(hero).toHaveProperty("teamwork");
+    });
+    expect(await response.statusCode).toBe(200);
+  });
+});
